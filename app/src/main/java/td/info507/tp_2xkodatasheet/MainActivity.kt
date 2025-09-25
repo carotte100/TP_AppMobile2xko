525476e5988c5b1a4076031ae00d556dd5eac80f
carotte100 <carottelenullos@gmail.com>
2025-09-25 20:15:59 +0200

    premier commit

--- app/src/main/java/td/info507/tp_2xkodatasheet/MainActivity.kt
+++ app/src/main/java/td/info507/tp_2xkodatasheet/MainActivity.kt
@@ -19,9 +19,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TP_2XKOdatasheetTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TP_2XKOdatasheetTheme {
+        Greeting("Android")
     }
 }
