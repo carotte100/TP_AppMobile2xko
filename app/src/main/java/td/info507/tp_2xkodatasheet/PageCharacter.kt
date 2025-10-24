a78b39b59b5bff3355cbf45c94abfc1f61b3aa19
carotte100 <carottelenullos@gmail.com>
2025-10-24 23:30:27 +0200

    lazy column pour scroll

--- app/src/main/java/td/info507/tp_2xkodatasheet/PageCharacter.kt
+++ app/src/main/java/td/info507/tp_2xkodatasheet/PageCharacter.kt
@@ -3,6 +3,7 @@ package td.info507.tp_2xkodatasheet
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -27,80 +28,96 @@ fun PageCharacter(champion: Champion, onBack: () -> Unit) {
     val imgName = champion.nom.lowercase() // nom de l'image
     val imgResId = context.resources.getIdentifier(imgName, "drawable", context.packageName)
 
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFF1C1C1E)),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .background(colorResource(R.color.noir)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        contentPadding = PaddingValues(bottom = 32.dp) // Ajout de marge en bas
     ) {
+        item {
 
-        Spacer(Modifier.height(32.dp))
-
-        // Header avec image de fond et contenu au-dessus
-        Box(
-            modifier = Modifier
-                .fillMaxWidth() // L'image prend toute la largeur
-                .height(180.dp) // taille du header
-        ) {
-            // Image de fond
-            val context = LocalContext.current
-            val imgName = champion.nom.lowercase()
-            val imgResId = context.resources.getIdentifier(imgName, "drawable", context.packageName)
-
-            if (imgResId != 0) {
-                Image(
-                    painter = painterResource(id = imgResId),
-                    contentDescription = "Fond du header",
-                    contentScale = ContentScale.Crop, // prend tout l'espace
-                    modifier = Modifier
-                        .fillMaxSize()
-                )
-            }
-
-            Box(
+            Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(Color(0x66000000)) // noir transparent
-            )
-
-            // Contenu flottant
-            Row(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.Top
+                    .background(colorResource(R.color.noir)),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = champion.nom,
-                    color = Color.White,
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(16.dp))
-                        .background(colorResource(R.color.vert).copy(alpha = 0.8f))
-                        .padding(horizontal = 12.dp, vertical = 6.dp)
-                )
 
-                Button(
-                    onClick = onBack,
-                    shape = RoundedCornerShape(50.dp),
-                    contentPadding = PaddingValues(0.dp),
+                Spacer(Modifier.height(32.dp))
+
+                // Header avec image de fond et contenu au-dessus
+                Box(
                     modifier = Modifier
-                        .size(40.dp)
+                        .fillMaxWidth()
+                        .height(180.dp)
                 ) {
-                    Image(
-                        painter = painterResource(R.drawable.cancel),
-                        contentDescription = "Retour",
-                        modifier = Modifier.fillMaxSize()
+                    // Image de fond
+                    val context = LocalContext.current
+                    val imgName = champion.nom.lowercase()
+                    val imgResId =
+                        context.resources.getIdentifier(imgName, "drawable", context.packageName)
+
+                    if (imgResId != 0) {
+                        Image(
+                            painter = painterResource(id = imgResId),
+                            contentDescription = "Fond du header",
+                            contentScale = ContentScale.Crop, // prend tout l'espace
+                            modifier = Modifier
+                                .fillMaxSize()
+                        )
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color(0x66000000)) // noir transparent
                     )
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.Top
+                    ) {
+                        Text(
+                            text = champion.nom,
+                            color = Color.White,
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(16.dp))
+                                .background(colorResource(R.color.vert).copy(alpha = 0.8f))
+                                .padding(horizontal = 12.dp, vertical = 6.dp)
+                        )
+
+                        Button(
+                            onClick = onBack,
+                            shape = RoundedCornerShape(50.dp),
+                            contentPadding = PaddingValues(0.dp),
+                            modifier = Modifier
+                                .size(40.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.cancel),
+                                contentDescription = "Retour",
+                                modifier = Modifier.fillMaxSize()
+                            )
+                        }
+                    }
                 }
             }
         }
-
-        Spacer(Modifier.height(12.dp))
-        DepliantDescr(modifier = Modifier.padding(horizontal = 16.dp), champion = champion)
-        Spacer(Modifier.height(12.dp))
-        DepliantCoup(modifier = Modifier.padding(horizontal = 16.dp), champion = champion)
-        Spacer(Modifier.height(12.dp))
-        DepliantCombo(modifier = Modifier.padding(horizontal = 16.dp), champion = champion)
+        item {
+            Spacer(Modifier.height(12.dp))
+            DepliantDescr(modifier = Modifier.padding(horizontal = 16.dp), champion = champion)
+        }
+        item {
+            Spacer(Modifier.height(12.dp))
+            DepliantCoup(modifier = Modifier.padding(horizontal = 16.dp), champion = champion)
+        }
+        item {
+            Spacer(Modifier.height(12.dp))
+            DepliantCombo(modifier = Modifier.padding(horizontal = 16.dp), champion = champion)
+        }
     }
 }
